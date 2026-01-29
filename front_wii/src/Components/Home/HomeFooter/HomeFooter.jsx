4f3d017f44b9bb3b3cfb68d5ca8ba0fde5090184
Florent Cretin <florent.cretin@hotmail.fr>
2026-01-29 13:29:51 +0100

    HomeFooter: stretch the block

--- front_wii/src/Components/Home/HomeFooter/HomeFooter.jsx
+++ front_wii/src/Components/Home/HomeFooter/HomeFooter.jsx
@@ -7,8 +7,10 @@ export default function HomeFooter() {
     return (
         <div className="grid_footer grid_fr1_fr2_fr1">
 
-                <div className="grid_icone iconecolor" style={{flex:1,gridColumnStart: 1,gridColumnEnd: 2,}}>
-                    contact (notre github)
+                <div className="grid_icone iconecolor" style={{gridColumnStart: 1,gridColumnEnd: 2}}>
+                    <a href="">
+                        contact (notre github)
+                    </a>
                 </div>
 
 
@@ -17,8 +19,10 @@ export default function HomeFooter() {
                 </div>
                 
 
-                <div className="grid_icone iconecolor" style={{flex:1,gridColumnStart: 3,gridColumnEnd: 4,}}>
-                    <a href="">Profile</a>
+                <div className="grid_icone iconecolor" style={{gridColumnStart: 3,gridColumnEnd: 4}}>
+                    <a href="">
+                        Profile
+                    </a>
                 </div>
 
         </div>
