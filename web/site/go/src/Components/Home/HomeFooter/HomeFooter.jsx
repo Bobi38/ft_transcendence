296dad9f5df51df42ee1d8a96271217740dfd530
Titouan Voisin <tvoisin@z1r8p3.42lyon.fr>
2026-01-29 12:00:10 +0100

    first

---
+++ web/site/go/src/Components/Home/HomeFooter/HomeFooter.jsx
@@ -0,0 +1,27 @@
+import "./HomeFooter.css"
+import "../Home.css"
+
+export default function HomeFooter() {
+    
+    
+    return (
+        <div className="grid_footer">
+            <div className="grid_fr1_fr2_fr1">
+
+                <div className="grid_icone iconecolor" style={{gridColumnStart: 1,gridColumnEnd: 2,}}>
+                    contact (notre github)
+                </div>
+
+                <div>
+                    <p>8:42</p>
+                </div>
+                
+                <div className="grid_icone iconecolor" style={{gridColumnStart: 3,gridColumnEnd: 4,}}>
+                    <a href="">Profile</a>
+                    
+                </div>
+
+            </div>
+        </div>
+    );
+}
