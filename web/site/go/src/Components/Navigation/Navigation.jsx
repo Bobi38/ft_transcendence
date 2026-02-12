0048d79ee5b78860bc4268a5701b75c9a548542f
Florent Cretin <florent.cretin@hotmail.fr>
2026-02-12 14:42:40 +0100

    import in navigation

--- web/site/go/src/Components/Navigation/Navigation.jsx
+++ web/site/go/src/Components/Navigation/Navigation.jsx
@@ -7,6 +7,8 @@ import NavBar from "./NavBar/NavBar.jsx";
 import ContactUs from "./Page/ContactUs/ContactUs.jsx";
 import Morpion from "./Page/Game/Morpion/Morpion.jsx";
 import WaitRoom from "./Page/WaitRoom/WaitRoom.jsx";
+import Stats from "./Page/Stats/Stats.jsx";
+import Nothing from "./Page/Nothing/Nothing.jsx";
     
 export default function Navigation({ screen }) {
 
@@ -19,6 +21,8 @@ export default function Navigation({ screen }) {
                 return <ContactUs/>;
             case 'Morpion':
                 return <Morpion/>;
+            case 'Stats':
+                return <Stats/>;
             case 'WaitRoom':
                 return <WaitRoom/>;
             default:
