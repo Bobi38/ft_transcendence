f563ac29f3b31f914d14d0f8454f3caf8a8e8156
Florent Cretin <florent.cretin@hotmail.fr>
2026-02-20 10:03:50 +0100

    refacto homemessage scss form conflict

--- web/site/go/src/Components/Home/HomeMessage/HomeMessage.jsx
+++ web/site/go/src/Components/Home/HomeMessage/HomeMessage.jsx
@@ -102,29 +102,29 @@ export default function HomeMessage({parent_style}) {
 
     return (
         <>
-            <div className={`${parent_style} HomeMessage-box`}>
+            <div className={`${parent_style} HomeMessage-root`}>
 
                     <div className="HomeMessage-message">
                         <h3>Chat</h3>
 
-                    {displayedMessages && displayedMessages.map((msg, index) => (
+                        {displayedMessages && displayedMessages.map((msg, index) => (
 
-                        <div  key={index} className={`full ${msg.monMsg ? "HomeMessage-me" : "HomeMessage-other"}`}>
+                            <div  key={index} className={`full ${msg.monMsg ? "HomeMessage-me" : "HomeMessage-other"}`}>
 
-                            {msg.monMsg ? (
-                                <>
-                                    <div><span>{msg.timer}</span></div>
-                                    <p>{msg.message}</p>
-                                </>
-                            ) : (
-                                <>
-                                    <div><strong>{msg.login}</strong> <span>{msg.timer}</span></div>
-                                    <p>{msg.message}</p>
-                                </>
-                            )}
+                                {msg.monMsg ? (
+                                    <>
+                                        <div><span>{msg.timer}</span></div>
+                                        <p>{msg.message}</p>
+                                    </>
+                                ) : (
+                                    <>
+                                        <div><strong>{msg.login}</strong> <span>{msg.timer}</span></div>
+                                        <p>{msg.message}</p>
+                                    </>
+                                )}
 
-                        </div>
-                    ))}
+                            </div>
+                        ))}
 
                     </div>
 
