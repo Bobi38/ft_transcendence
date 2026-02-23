e3dde82dc232218de3f6eb3a41de327a1bc638ae
Corbiobio <113342618+Corbiobio@users.noreply.github.com>
2026-02-23 20:06:07 +0100

    improved home css

--- web/front/src/page/Home/HomeCard/HomeCard.jsx
+++ web/front/src/page/Home/HomeCard/HomeCard.jsx
@@ -3,12 +3,20 @@ import "./HomeCard.scss";
 
 /* Components */
     
+
+
+
+
 export default function HomeCard({children, path}) {
+
+
+
+
 
     // le boutton connard ? je l'ai oublier ps: c'est moi meme
 
     return (
-        <a className={`HomeCard`} href={path}>
+        <a className={`HomeCard card-effect`} href={path}>
             {children}
         </a>
     )
