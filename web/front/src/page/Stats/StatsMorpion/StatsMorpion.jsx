03a6d397338056d82fa375eb810d78ca9dd8302b
Florent Cretin <florent.cretin@hotmail.fr>
2026-02-23 13:24:42 +0100

    je sais pas

--- web/front/src/page/Stats/StatsMorpion/StatsMorpion.jsx
+++ web/front/src/page/Stats/StatsMorpion/StatsMorpion.jsx
@@ -5,7 +5,40 @@ import "./StatsMorpion.scss";
     
 export default function StatsMorpion() {
 
-    
+    const data = {
+        my_id: 1,
+        history: [
+            {
+                gameid: 1,
+                game_stats:{
+                    
+                    map: "OX--XO-XO",
+                    //has_result: {same winner_id = green / not same winner_is = red}, "draw,abort" = blue
+                    game_result: "has_result,draw,abort",
+                    id_user: {
+                        //null !has_result
+                        winner_id: 1,//if is not my id i have lost
+                        player: [1, 2]
+                    },
+                }
+            },
+            {
+                gameid: 2,
+                game_stats:{
+                    
+                    map: "",
+                    //has_result: {same winner_id = green / not same winner_is = red}, "draw,abort" = blue
+                    game_result: "has_result,draw,abort",
+                    id_user: {
+                        //null !has_result
+                        winner_id: 2,//if is not my id i have lost
+                        player: [1, 2]
+                    },
+                }
+            }
+        ]
+    }
+
     return (
         <div className={`StatsMorpion-root`}>
 
