e46215b044420db320ef66ab7facda183538d1fd
sflechel <solalflechelles@gmail.com>
2026-03-05 12:03:55 +0100

    discrepancies between server side headless sim and client side render eliminated

--- web/back/gameServer/src/rooms/MyRoom.ts
+++ web/back/gameServer/src/rooms/MyRoom.ts
@@ -1,6 +1,6 @@
 import { Room, Client, CloseCode } from "colyseus";
 import { MyRoomState } from "./schema/MyRoomState.js";
-import { HavokPlugin, MeshBuilder, NullEngine, PhysicsBody, PhysicsMotionType, PhysicsShape, PhysicsShapeBox, PhysicsShapeSphere, Quaternion, Scene, TransformNode, Vector3 } from "@babylonjs/core"
+import { ArcRotateCamera, HavokPlugin, MeshBuilder, NullEngine, PhysicsBody, PhysicsMotionType, PhysicsShape, PhysicsShapeBox, PhysicsShapeSphere, Quaternion, Scene, TransformNode, Vector3 } from "@babylonjs/core"
 import HavokPhysics from "@babylonjs/havok";
 import { Env } from "../../../../media/media.js"
 import fs from "fs";
@@ -21,6 +21,9 @@ export class MyRoom extends Room {
   private _engine: NullEngine;
   private _ballNode : TransformNode;
   private _bodies: PhysicsBody[] = [];
+  private _shapes: PhysicsShape[] = [];
+  private _nodes: TransformNode[] = [];
+  private _havokPlugin: HavokPlugin;
 
   maxClients = 4;
   state = new MyRoomState();
@@ -38,13 +41,15 @@ export class MyRoom extends Room {
     const ballPos = new Vector3(this.state.ball.position.x, this.state.ball.position.y, this.state.ball.position.z);
     const ballNode = new TransformNode("ballNode", this._scene);
     ballNode.position = ballPos;
-    const ballShape = new PhysicsShapeSphere(Vector3.Zero(), 1, this._scene);
+    const ballShape = new PhysicsShapeSphere(Vector3.Zero(), 0.5, this._scene);
     const ball = new PhysicsBody(ballNode, PhysicsMotionType.DYNAMIC, false, this._scene);
     ball.shape = ballShape;
     this._ballNode = ballNode;
     const material = {friction: 0, restitution: 1};
     ballShape.material = material;
     ball.setMassProperties({mass: 1});
+    ball.setLinearDamping(0);
+    ball.setAngularDamping(0);
     this._bodies.push(ball);
     return ball;
   }
@@ -78,16 +83,48 @@ export class MyRoom extends Room {
     ground.shape = groundShape;
     ceiling.shape = ceilingShape;
     elevan.shape = elevanShape;
+    const wallmaterial = {friction: 0, restitution: 1};
+    wallRightShape.material = wallmaterial;
+    wallLeftShape.material = wallmaterial;
+    groundShape.material = wallmaterial;
+    ceilingShape.material = wallmaterial;
+    elevanShape.material = wallmaterial;
+    wallRight.setMassProperties({mass: 1});
+    wallLeft.setMassProperties({mass: 1});
+    ground.setMassProperties({mass: 1});
+    ceiling.setMassProperties({mass: 1});
+    elevan.setMassProperties({mass: 1});
+    wallRight.setLinearDamping(0);
+    wallLeft.setLinearDamping(0);
+    ground.setLinearDamping(0);
+    ceiling.setLinearDamping(0);
+    elevan.setLinearDamping(0);
+    wallRight.setAngularDamping(0);
+    wallLeft.setAngularDamping(0);
+    ground.setAngularDamping(0);
+    ceiling.setAngularDamping(0);
+    elevan.setAngularDamping(0);
     this._bodies.push(wallLeft);
     this._bodies.push(wallRight);
     this._bodies.push(ground);
     this._bodies.push(ceiling);
     this._bodies.push(elevan);
+    this._shapes.push(wallRightShape);
+    this._shapes.push(wallLeftShape);
+    this._shapes.push(groundShape);
+    this._shapes.push(ceilingShape);
+    this._shapes.push(elevanShape);
+    this._nodes.push(wallRightNode);
+    this._nodes.push(wallLeftNode);
+    this._nodes.push(groundNode);
+    this._nodes.push(ceilingNode);
+    this._nodes.push(elevanNode);
   }
 
   private async _startSimulation() {
     const engine = new NullEngine();
     const scene = new Scene(engine);
+    const camera = new ArcRotateCamera("Camera", 0, 0.8, 100, Vector3.Zero(), scene); //necessary for scene.render()
     const wasmPath = path.resolve("node_modules/@babylonjs/havok/lib/esm/HavokPhysics.wasm");
     const wasmBuffer = fs.readFileSync(wasmPath);
     const wasmBinary = wasmBuffer.buffer.slice(wasmBuffer.byteOffset,wasmBuffer.byteOffset + wasmBuffer.byteLength);
@@ -97,6 +134,7 @@ export class MyRoom extends Room {
     //     });
     console.log("HavokPhysics loaded from file");
     const havokPlugin = new HavokPlugin(true, havok);
+    this._havokPlugin = havokPlugin;
     const deltaTime = 1 / 60;
     havokPlugin.setTimeStep(deltaTime);
     scene.enablePhysics(new Vector3(0, 0, 0), havokPlugin); //no gravity (middle value at 0)
@@ -107,11 +145,20 @@ export class MyRoom extends Room {
     ball.setLinearVelocity(new Vector3(0,0,1))
 
     //manually stepping
-    setInterval(() => {
+    // setInterval(() => {
+    //   console.log(this._ballNode.position);
+    //   console.log(ball.getLinearVelocity());
+    //   havokPlugin.executeStep(deltaTime, this._bodies);
+    // }, deltaTime * 1000);
+
+    scene.onBeforePhysicsObservable.add(() => {
       console.log(this._ballNode.position);
       console.log(ball.getLinearVelocity());
-      havokPlugin.executeStep(deltaTime, this._bodies);
-    }, deltaTime * 1000);
+    });
+
+    engine.runRenderLoop(() => {
+      scene.render();
+    })
 }
 
   onCreate (options: any) {
@@ -131,7 +178,12 @@ export class MyRoom extends Room {
 
   onLeave (client: Client, code: CloseCode) {
     /**
-     * Called when a client leaves the room.
+     * Called when a client leaves the room.//manually stepping
+    setInterval(() => {
+      console.log(this._ballNode.position);
+      console.log(ball.getLinearVelocity());
+      havokPlugin.executeStep(deltaTime, this._bodies);
+    }, deltaTime * 1000);
      */
     console.log(client.sessionId, "left!", code);
   }
@@ -140,7 +192,27 @@ export class MyRoom extends Room {
     /**
      * Called when the room is disposed.
      */
-    console.log("room", this.roomId, "disposing...");
+    this._bodies.forEach(body => {
+        if (body.shape) {
+            body.shape.dispose();
+        }
+        body.dispose(); 
+    });
+    this._bodies = [];
+
+    this._nodes.forEach(node => node.dispose());
+    this._nodes = [];
+    //don't forget to remove Observables if any
+
+    if (this._havokPlugin) {
+        this._havokPlugin.dispose();
+        this._havokPlugin = null;
+    }
+    if (this._scene) {
+        this._scene.dispose();
+        this._scene = null;
+    }
+    console.log("room", this.roomId, "disposing and ending simulation");
   }
 
 }
