9145d73401249c4478db935ebfa33cd17a7a389d
Florent Cretin <florent.cretin@hotmail.fr>
2026-03-07 13:42:56 +0100

    more refactor on morpion jsx logique

--- web/front/src/page/all_game/Morpion/Morpion.jsx
+++ web/front/src/page/all_game/Morpion/Morpion.jsx
@@ -2,35 +2,6 @@ import { useState, useEffect } from "react";
 import SocketM from "/app/front/tool/SocketManag.js";
 import './Morpion.scss';
 
-function GoOUT(){
-
-    return (
-        <button onClick={() =>{
-            SocketM.sendd({
-                type: "game",
-                message: "je pars"
-            })
-        }}>
-            je veux partir
-        </button>
-    );
-}
-
-function NouvellePartie(){
-
-    return (
-        <button onClick={() => {
-            SocketM.sendd({
-                type: "game",
-                message: "je veux jouer"
-            })
-        }}>
-            Nouvelle Partie
-        </button>
-    );
-}
-
-
 function RebootTruc() {
 
     return (
@@ -41,6 +12,20 @@ function RebootTruc() {
             })
         }}>
             Reboot (dev - ne pas utiliser - a supprimer en prod)
+        </button>
+    );
+}
+
+function GoOut(){
+
+    return (
+        <button onClick={() =>{
+            SocketM.sendd({
+                type: "game",
+                message: "je pars"
+            })
+        }}>
+            je veux partir
         </button>
     );
 }
@@ -58,16 +43,22 @@ function SelectFirst(){
     ); 
 }
 
-function Square({ children, onSquareClick }) {
+function NouvellePartie({ setBoard }){
 
     return (
-        <button className="square" onClick={onSquareClick}>
-            {children}
+        <button onClick={() => {
+            setBoard(Array(9).fill(""));
+            SocketM.sendd({
+                type: "game",
+                message: "je veux jouer"
+            })
+        }}>
+            Nouvelle Partie
         </button>
     );
 }
 
-function Board( { squares }) {
+function Board({ board }) {
 
     function handleClick(i) {
         SocketM.sendd({
@@ -75,77 +66,69 @@ function Board( { squares }) {
             message: i,
         })
     }
-  
+
     return (
-        // <div className={`Board-root`}>
-        //     <div className="Board-row">
-        //         <Square onSquareClick={() => handleClick(0)}> {squares[0]} </Square>
-        //         <Square onSquareClick={() => handleClick(1)}> {squares[1]} </Square>
-        //         <Square onSquareClick={() => handleClick(2)}> {squares[2]} </Square>
-        //     </div>
-        //     <div className="Board-row">
-        //         <Square onSquareClick={() => handleClick(3)}> {squares[3]} </Square>
-        //         <Square onSquareClick={() => handleClick(4)}> {squares[4]} </Square>
-        //         <Square onSquareClick={() => handleClick(5)}> {squares[5]} </Square>
-        //     </div>
-        //     <div className="Board-row">
-        //         <Square onSquareClick={() => handleClick(6)}> {squares[6]} </Square>
-        //         <Square onSquareClick={() => handleClick(7)}> {squares[7]} </Square>
-        //         <Square onSquareClick={() => handleClick(8)}> {squares[8]} </Square>
-        //     </div>
-        // </div>
         <div className={`Board-root`}>
-                <Square onSquareClick={() => handleClick(0)}> {squares[0]} </Square>
-                <Square onSquareClick={() => handleClick(1)}> {squares[1]} </Square>
-                <Square onSquareClick={() => handleClick(2)}> {squares[2]} </Square>
-                <Square onSquareClick={() => handleClick(3)}> {squares[3]} </Square>
-                <Square onSquareClick={() => handleClick(4)}> {squares[4]} </Square>
-                <Square onSquareClick={() => handleClick(5)}> {squares[5]} </Square>
-                <Square onSquareClick={() => handleClick(6)}> {squares[6]} </Square>
-                <Square onSquareClick={() => handleClick(7)}> {squares[7]} </Square>
-                <Square onSquareClick={() => handleClick(8)}> {squares[8]} </Square>
+
+            {board?.map((element, index) => (
+
+                <button key={index} className={`square`} 
+                        onClick={() => handleClick(index)}>
+
+                    {element}
+                </button>
+
+            ))}
+
         </div>
     );
 }
 
 export default function Morpion() {
 
-    console.log("ici cest front FunctionMorpion");
+
     const [msg, setMsg] = useState("En attente...");
-    const [board, setBoard] = useState(Array(9).fill(" "));
+    const [board, setBoard] = useState(null);
 
       useEffect(() => {
 
-      // SocketM.sendd({
-      //       type: "game",
-      //       message: "est-ce que tu me reçois ?"
-      // });
-      
-      console.log("after co");
+        console.log("Morpion component called");
 
-      const handleTest = (data) => {
-          console.log(data)
-          setMsg(data.message);
-          if (data.board)
-              setBoard(data.board);
-      };
+        // SocketM.sendd({
+        //       type: "game",
+        //       message: "est-ce que tu me reçois ?"
+        // });
+        
+        console.log("Morpion component after connection");
 
-      SocketM.onGame(handleTest);
+        const handleTest = (data) => {
+         
+            console.log("Morpion component handleTest data:", data)
+            if (data.message !== msg){
+                setMsg(data.message);
+            }
+            if (data.board && data.board !== board){
+                setBoard(data.board);
+            }
+        };
 
-      return () => {
-          SocketM.offGame(handleTest);
-      }
+        SocketM.onGame(handleTest);
+
+        return () => {
+            SocketM.offGame(handleTest);
+        }
+
     }, []);
 
-      // useEffect(() => {
+    // useEffect(() => {
 
-      //   return (() => {
-      //         SocketM.sendd({
-      //             type: "game",
-      //             message: "je pars"
-      //         })
-      //     });
-      // }, [])
+    //   return (() => {
+    //         SocketM.sendd({
+    //             type: "game",
+    //             message: "je pars"
+    //         })
+    //     });
+    // }, [])
 
     return (
         <div className={`Morpion-root`}>
@@ -155,17 +138,17 @@ export default function Morpion() {
                 
                 <RebootTruc/>
             
-                <GoOUT/>
+                <GoOut/>
             
                 <SelectFirst/>
 
             </div>
   
 
-            <Board squares={board}/>
+            <Board board={board}/>
   
 
-            <NouvellePartie/>
+            <NouvellePartie setBoard={setBoard}/>
 
         </div>
     );
