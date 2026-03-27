0aa42de4486741a2e1ace74d8d514f916ca3b7b7
Corbiobio <113342618+Corbiobio@users.noreply.github.com>
2026-03-27 16:53:36 +0100

    remove home footer and add code eto navbar

--- web/front/src/Component/NavBar/NavBar.jsx
+++ web/front/src/Component/NavBar/NavBar.jsx
@@ -4,8 +4,39 @@ import "./NavBar.scss";
 /* Components */
 import Button from "FRONT/Component/Button/Button.jsx"
 import useClock from "FRONT/hooks/useClock.jsx";
+import { AUTH } from "TOOL/AuthContext.jsx";
+import  SocketM  from "TOOL/SocketManag.js"
 
 export default function NavBar() {
+
+	//TODO Remettre le logout
+
+	//function logout() {
+	//    console.log("logout(1) called")
+	//    fetch('/api/auth/logout', {
+	//        method: 'GET',
+	//        headers: { 'Content-Type': 'application/json' },
+	//        credentials: "include"
+	//    })
+	//    .then(response => response.json())
+	//    .then(data => {
+
+	//        if (data.success) {
+	//            setShowLog(AUTH.LOGIN);
+	//            // SocketM.sendd('friend', {type: "logout"});
+	//            SocketM.disconnect('friend');
+	//            SocketM.disconnect('morp');
+	//            SocketM.disconnect('priv');
+	//            SocketM.disconnect('chat');
+	//        } else {
+	//            console.error("logout(2) failed");
+	//        }
+
+	//    })
+	//    .catch(error => {
+	//        console.error("logout(3) Error:", error);
+	//    });
+	//}
 
     const time = useClock();
 
@@ -18,9 +49,9 @@ export default function NavBar() {
 				<p>{time}</p>
 
 				{/*onClick={(logout)}*/}
-				<button className={`logout`}>
+				{/*<button className={`logout`}>
 					Logout
-				</button>
+				</button>*/}
 			</div>
 
 			<Button path={`/ContactUs`}>Contact Us</Button>
