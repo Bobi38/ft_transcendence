407bcbc13f1bfee7ffd588cc00e7ed95b92d12b0
Corbiobio <113342618+Corbiobio@users.noreply.github.com>
2026-03-27 15:53:17 +0100

    fix conflict head in home

--- web/front/src/page/Home/Home.jsx
+++ web/front/src/page/Home/Home.jsx
@@ -1,6 +1,6 @@
 /* extern */
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { AUTH, useAuth } from "TOOL/AuthContext.jsx"
 
 /* back */
 import checkCo from "TOOL/fonction_usefull.js"
