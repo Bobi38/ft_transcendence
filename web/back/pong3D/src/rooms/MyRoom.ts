6af98f2bbd16aa50a2b2d8e604023befa4a89032
sflechel <solalflechelles@gmail.com>
2026-04-09 12:15:37 +0200

    fixed bug when winning game and leaving room

--- web/back/pong3D/src/rooms/MyRoom.ts
+++ web/back/pong3D/src/rooms/MyRoom.ts
@@ -206,6 +206,7 @@ export class MyRoom extends Room {
     const player = this.state.players.get(client.sessionId);
     if (player) player.connected = false;
     
+    console.log("onLeave:", this.state.roomStatus);
     if (this.state.roomStatus === RoomStatus.STARTED) {
       this.state.roomStatus = RoomStatus.AWAITING_RECONNECTION;
     }
@@ -216,6 +217,7 @@ export class MyRoom extends Room {
       console.log(`${client.sessionId} reconnected successfully!`);
       if (player) player.connected = true;
 
+      console.log("on reconnection",this.state.roomStatus);
       if (this.state.roomStatus === RoomStatus.AWAITING_RECONNECTION) {
         this.state.roomStatus = RoomStatus.STARTED;
       }
@@ -233,12 +235,14 @@ export class MyRoom extends Room {
         activePlayers.delete(stats.id);
     }
 
-    if (this.state.roomStatus !== RoomStatus.PLAYER_DISCONNECTED) {
+    const state = this.state.roomStatus;
+    if (state == RoomStatus.STARTED || state == RoomStatus.AWAITING_RECONNECTION || state == RoomStatus.WAITING) {
         this.state.roomStatus = RoomStatus.PLAYER_DISCONNECTED;
-        this._timeEnd = this._timeEnd || Date.now();
-        this._simulation?.getEngine()?.stopRenderLoop();
-        this.lock();
     }
+    
+    this._timeEnd = this._timeEnd || Date.now();
+    this._simulation?.getEngine()?.stopRenderLoop();
+    this.lock();
   } 
 
   async _sendGameDataToDatabase() {
