e3a65c6412518fd459a50fa651ef19a47a8b49ef
ncls42 <ncls42@example.com>
2026-04-10 14:48:39 +0200

    protect websocket in morpion

--- web/back/morpion/src/morpion/PlayMorpion.js
+++ web/back/morpion/src/morpion/PlayMorpion.js
@@ -13,7 +13,7 @@ const msgs = {
     draw: "It's a draw!",
     second: "You are playing second",
     first: "You are playing first",
-    reboot: "Server has rebooted", // tres utile !!
+    reboot: "Server has rebooted",
     badMove: "Move alert",
 }
 
@@ -80,11 +80,11 @@ function move(player, move){
     }
 }
 
-function searchGame(player, players){
+function searchGame(player){
 
     let game = player.getGame();
     if (game?.isState("play")){
-        player.send(`you play ${game.getId()}`);
+        player.sendMessage();
         return false;
     }
 
@@ -100,7 +100,6 @@ function searchGame(player, players){
 
     managerRoom.refreshList();
 
-    // players.forEach(p => {p.sendList();});
     game.notifyTurn(
         {message: msgs.my_turn, turn: true},
         {message: msgs.other_turn, turn: false}
@@ -110,8 +109,12 @@ function searchGame(player, players){
 }
 
 function reboot(){
+
+    if (process.env.STATUS !== "DEV") return;
+
     managerRoom.removeAll(msgs.reboot);
     managerRoom.refreshList();
+
     return;
 }
 
@@ -144,99 +147,5 @@ function updateName(player, nickname) {
 
     managerRoom.refreshList();
 }
-
-const cooldowns = new Map();
-
-// function morpion(message, socket){
-//     const id = socket.userId;
-
-//     if (cooldowns.has(id)) return;
-
-//     cooldowns.set(id, true);
-//     setTimeout(() => cooldowns.delete(id), 200);
-
-//     // console.log(`connecte dans play Morpion ${message} de ${id}`)
-
-//     let game = manager_room.isInRoom(id);
-
-//     if (message === "reboot") {
-//         manager_room.removeAll("le serveur a reboot");
-//         return;
-//     }
-
-//     if (message === "je veux jouer") {
-//         console.log("nouvelle demande");
-//         if (game){
-//             console.log("a deja une partie");
-//             socket.send(JSON.stringify({type: "game", message: `ca peut etre tres long :${game}}`}))
-//             return ;
-//         }
-
-//         game = manager_room.findOnePlace(socket, "Morpion", id);
-//         // manager_room.lobby.removePlayer(id);
-//         socket.send(JSON.stringify({type: "game", message: game.getId()}))
-//         try{
-//             game.setLock(true);
-//             game.startGame(id);
-
-//             game.notifyTurn(
-//                 { message: "À toi de jouer", turn: true },
-//                 { message: "Tour adverse", turn: false });
-//         }
-//         catch (err) {
-//             console.log("premier set _Turn");
-//             game._turn = id;
-//             socket.send(JSON.stringify({type: "game", message: "recherche"}))
-//         }
-//         return;
-//     }
-
-//     if (!game) return ;
-
-//     if (message === "leave") {
-
-//         if (!game.getLock()) {
-//             manager_room.removePlayer(id, "bye bye");
-//             return;
-//         }
-
-//         if (id === game.getTurn())
-//             game.switchTurn;
-//         game.handleEndGame('abort', game.getTurn());
-//         game.notifyTurn(
-//             { message: "Abondon - tu as gagne", turn: false },
-//             { message: "Abondon - tu as perdu", turn: false });
-//         manager_room.removeRoom(game.getId());
-//         return ;
-//     }
-
-//     if (message === "playSecond") {
-
-//         if (game.setFirstPlayer())
-//             socket.send(JSON.stringify({ type: "game", message: `${game} : tu joues en second` }))
-//         return;
-//     }
-
-//     if (!game.getLock()) return;
-
-//     if (!game.isTurnPlayer(id)) return;
-
-//     console.log(`je suis dans ${game}`);
-//     if (game.play(id, message)) {
-//         if(game.checkVictory()){
-//             console.log(`fin de la partie de ${game}`);
-//             manager_room.removeRoom(game.getId());
-//             return ;
-//         }
-//         game.switchTurn();
-//         console.log(` joueur ${id} a jouer sur la case ${message}`)
-//         game.notifyTurn(
-//             { message: "À toi de jouer", turn: true },
-//             { message: "Tour adverse", turn: false });
-//         game.startTurnTimer();
-//         console.log(`le changement`);
-//     }
-
-// }
 
 export default {reboot, playSecond, leave, msgs, searchGame, move, observator, updateName}
