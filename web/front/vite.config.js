197a1bd3403f68460ac8eb4f3238f72c3e57fd32
Corbiobio <113342618+Corbiobio@users.noreply.github.com>
2026-04-15 15:21:28 +0200

    add .42lyon to allowedHosts in vite conf for dev

--- web/front/vite.config.js
+++ web/front/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-    host: '0.0.0.0', allowedHosts: ['fcretin.ddns.net','z3r3p5','z3r3p5.42lyon.fr'],
+    host: '0.0.0.0', allowedHosts: ['fcretin.ddns.net', '.42lyon.fr'],
     proxy: {
   '/api': {
     target: 'http://gateway:9000',
